eab9d45a4903f804f9482db87572d63aa7753353
SecretPocketCat <jiridexterhanc@gmail.com>
2022-04-14 18:03:34 +0200

    RemovedComponents repro

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .run();
+        .add_system_to_stage(CoreStage::First, remove);
+
+    for stage in [
+        CoreStage::First,
+        CoreStage::PreUpdate,
+        CoreStage::Update,
+        CoreStage::PostUpdate,
+        CoreStage::Last,
+    ]
+    .iter()
+    {
+        app.add_system_to_stage(stage.clone(), log_removed(stage.clone()));
+    }
+
+    app.run();
 }
 
+#[derive(Component)]
+struct ToBeRemoved;
+
 fn setup(mut commands: Commands) {
-    info!("Hi");
+    commands.spawn().insert(ToBeRemoved);
+}
+
+fn remove(mut commands: Commands, q: Query<Entity, With<ToBeRemoved>>) {
+    for e in q.iter() {
+        commands.entity(e).remove::<ToBeRemoved>();
+    }
+}
+
+fn log_removed(stage: CoreStage) -> impl Fn(RemovedComponents<ToBeRemoved>) {
+    move |removed| {
+        let count = removed.iter().len();
+        if count > 0 {
+            info!("Removed {count} components in {stage:?}");
+        }
+    }
 }
