55186e2232caf9d5b1032c4aa3c933ba46b9922e
SecretPocketCat <jiridexterhanc@gmail.com>
2022-04-24 11:20:27 +0200

    log startup

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,16 @@ fn main() {
         .add_system_to_stage(CoreStage::First, remove);
 
     for stage in [
+        StartupStage::PreStartup,
+        StartupStage::Startup,
+        StartupStage::PostStartup,
+    ]
+    .iter()
+    {
+        app.add_startup_system_to_stage(stage.clone(), log_startup_count(stage.clone()));
+    }
+
+    for stage in [
         CoreStage::First,
         CoreStage::PreUpdate,
         CoreStage::Update,
@@ -44,5 +54,12 @@ fn log_removed(
         if count > 0 || query_count > 0 {
             info!("Removed: {count}; Query count: {query_count} in {stage:?}");
         }
+    }
+}
+
+fn log_startup_count(stage: StartupStage) -> impl Fn(Query<(), With<ToBeRemoved>>) {
+    move |query| {
+        let query_count = query.iter().count();
+        info!("Query count during startup: {query_count} in {stage:?}");
     }
 }
